21628cee69a3c6a1d47025fc381aeeb2449ea9ef
ilya latushko <ilyalatt@gmail.com>
2020-07-15 13:23:52 +0300

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+./generate-dto.sh
+
 cd Telega
 rm -rf bin
-./generate-dto.sh
+
 dotnet pack
 dotnet nuget push bin/Debug/*.nupkg --source http://api.nuget.org/v3/index.json --api-key $TELEGA_NUGET_API_KEY
