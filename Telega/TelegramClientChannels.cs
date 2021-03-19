ea8890214266474d6ea6f3e613c40ba06c4e53b4
Georgy <tarasverq@gmail.com>
2021-03-19 06:37:08 +0300

    GetMessages and GetMessageById methods

--- Telega/TelegramClientChannels.cs
+++ Telega/TelegramClientChannels.cs
@@ -1,8 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using LanguageExt;
 using Telega.Connect;
 using Telega.Rpc.Dto.Functions.Channels;
 using Telega.Rpc.Dto.Types;
+using Telega.Rpc.Dto.Types.Messages;
 
 namespace Telega
 {
@@ -17,5 +20,49 @@ namespace Telega
             await _tg.Call(new JoinChannel(
                 channel: channel
             ));
+
+        public async Task<Messages> GetMessages(
+            Some<InputChannel> channel,
+            Arr<InputMessage> messages
+        ) =>
+            await _tg.Call(new GetMessages(
+                channel: channel,
+                id: messages
+            ));
+        
+        /// <summary>
+        /// This method can be used for getting messages by Id (including albums)
+        /// </summary>
+        /// <param name="channel"></param>
+        /// <param name="messageId"></param>
+        /// <returns>List of tags associated with this mesageId</returns>
+        public async Task<Arr<Message.Tag>> GetMessageById(
+            Some<InputChannel> channel,
+            int messageId
+        )
+        {
+            Messages messagesResponse =
+                await GetMessages(channel: channel,
+                    //We have to get 10 messages around required Id because of grouped messages (albums)
+                    messages: Enumerable.Range(start: -10, count: 20)
+                        .Select(selector: x => (InputMessage) new InputMessage.IdTag(id: messageId + x))
+                        .ToArr()
+                );
+            
+            List<Message.Tag> tags =
+                messagesResponse
+                    .AsChannelTag().Head()
+                    .Messages
+                    .Choose(Message.AsTag)
+                    .ToList();
+
+            Message.Tag mainMessage = tags.First(x => x.Id == messageId);
+
+            return mainMessage.GroupedId
+                    .Match(Some: groupedId =>
+                            tags.Where( tag => tag.GroupedId == groupedId).OrderBy(x => x.Id).ToArr(),
+                        None: () => { return new[] {mainMessage}; })
+                ;
+        }
     }
 }
