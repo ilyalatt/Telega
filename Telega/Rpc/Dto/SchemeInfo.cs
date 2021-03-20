075b3aebdcc037d74127027bd04ec07543c51ffc
Mohammad Babayi <chief.mb1976@gmail.com>
2021-03-20 11:37:50 +0330

    int code gen ReferenceEquals(*, null) => * is null

--- Telega/Rpc/Dto/SchemeInfo.cs
+++ Telega/Rpc/Dto/SchemeInfo.cs
@@ -1,7 +1,13 @@
+using System;
+using System.IO;
+using LanguageExt;
+using static Telega.Rpc.TgMarshal;
 using T = Telega.Rpc.Dto.Types;
 
-namespace Telega.Rpc.Dto {
-    static class SchemeInfo {
+namespace Telega.Rpc.Dto
+{
+    static class SchemeInfo
+    {
         public const int LayerVersion = 124;
     }
 }
