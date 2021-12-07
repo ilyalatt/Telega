8162034e44af36444c65e93fb397132d2fe57053
Ilya Latushko <ilyalatt@gmail.com>
2021-12-07 23:18:34 +0300

    new release

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ set -e
 
 cd Telega
 rm -rf bin
+dotnet pack --configuration Release
+dotnet nuget push bin/Release/*.nupkg --source http://api.nuget.org/v3/index.json --api-key $ILYALATT_NUGET_API_KEY
 
-dotnet pack
-dotnet nuget push bin/Debug/*.nupkg --source http://api.nuget.org/v3/index.json --api-key $ILYALATT_NUGET_API_KEY
